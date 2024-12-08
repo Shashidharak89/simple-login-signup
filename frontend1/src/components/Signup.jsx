af89527e20dd9eac204cd688de752702934f0e14
Shashidhara K <shashidharak334@gmail.com>
2024-12-08 23:22:05 +0530

    Update Signup.jsx

--- frontend1/src/components/Signup.jsx
+++ frontend1/src/components/Signup.jsx
@@ -20,7 +20,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch("https://sign-up-backend-uws4.onrender.com/api/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
